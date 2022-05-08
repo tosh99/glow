635bed7c105aeff5824ed7df153da286a986b55a
Anutosh <contact@localwell.in>
2022-05-08 20:25:19 +0530

    Header Final

--- shared/components/page-header/page-h2.js
+++ shared/components/page-header/page-h2.js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import styles from "./page-header.module.scss";
 import Link from "next/link";
+import ServicesMenu from "../services-menu/services-menu";
 
 export default function PageH2({ title, bg = "solid" }) {
     const [show_menu, set_show_menu] = useState(false);
@@ -9,34 +10,44 @@ export default function PageH2({ title, bg = "solid" }) {
     const router = useRouter();
 
     return (
-        <div
-            className={`outer ${styles.headerOuter} ${bg === "transparent" ? styles.headerOuterTrans : ""}`}
-            onClick={(ev) => {
-                console.log("EV1", ev);
-            }}
-        >
-            <div className={"inner " + styles.header}>
-                <div className={styles.hLeft}>
-                    <Link href="/">
-                        <img src={"/icons/header/back.svg"} />
-                    </Link>
-                </div>
-                <div
-                    className={styles.hRight}
-                    onClick={(ev) => {
-                        console.log("EV2", ev);
-                    }}
-                >
-                    <header>{title}</header>
-                    <img
-                        src={"/icons/header/star.svg"}
+        <>
+            <div
+                className={`outer ${styles.headerOuter} ${bg === "transparent" ? styles.headerOuterTrans : ""}`}
+                onClick={(ev) => {
+                    console.log("EV1", ev);
+                }}
+            >
+                <div className={"inner " + styles.header}>
+                    <div className={styles.hLeft}>
+                        <Link href="/">
+                            <img src={"/icons/header/back.svg"} />
+                        </Link>
+                    </div>
+                    <div
+                        className={styles.hRight}
                         onClick={(ev) => {
-                            console.log(ev);
-                            set_show_services_menu((prev) => !prev);
+                            console.log("EV2", ev);
                         }}
-                    />
+                    >
+                        <header>{title}</header>
+                        <img
+                            src={"/icons/header/star.svg"}
+                            onClick={(ev) => {
+                                console.log(ev);
+                                set_show_services_menu((prev) => !prev);
+                            }}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            {show_services_menu && (
+                <ServicesMenu
+                    close={() => {
+                        console.log("Closed");
+                        set_show_services_menu(false);
+                    }}
+                />
+            )}
+        </>
     );
 }
